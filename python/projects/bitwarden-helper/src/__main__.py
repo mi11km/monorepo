d3f15cea11f935a8e27e651aa79779e699dcb9bb
mi11km <mi11km.program@gmail.com>
2023-11-12 10:58:11 +0900

    create slack apps, socket mode

--- python/projects/bitwarden-helper/src/__main__.py
+++ python/projects/bitwarden-helper/src/__main__.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 import logging
+import os
 
 from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+logging.basicConfig(level=logging.DEBUG)
+
+slack_bot_token = os.environ["SLACK_BOT_TOKEN"]
+slack_app_token = os.environ["SLACK_APP_TOKEN"]
+
+app = App(token=slack_bot_token)
+
+
+@app.message("hello")
+def message_hello(message, say):
+    # イベントがトリガーされたチャンネルへ say() でメッセージを送信します
+    say(f"Hey there <@{message['user']}>!")
+
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    app = App()
-    app.start(3000)  # POST http://localhost:3000/slack/events
+    SocketModeHandler(app, slack_app_token).start()
